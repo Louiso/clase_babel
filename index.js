eb57af8867dd249ee0dc2124fdfe4b727c568b45
LuisSullca <Gransullca.25@gmail.com>
2018-03-24 18:48:58 -0500

    already

---
+++ index.js
@@ -0,0 +1,12 @@
+import queso from './funciones';
+
+const mensaje = 'Venecas';
+
+console.log('Hola '+mensaje);
+
+console.log(`Hola ${mensaje}`);
+
+'Hola ' + mensaje // lo que se espera 'Hola Venecas'
+
+console.log(queso.suma(1,2));
+console.log(queso.resta(1,2));
